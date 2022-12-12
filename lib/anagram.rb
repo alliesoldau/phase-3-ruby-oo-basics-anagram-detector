6f5c9e3c7ac54beb7232a62ceb454621f1a28b08
Allie Soldau <alliesoldau@gmail.com>
2022-12-12 14:22:58 -0500

    complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|value| value.split("").sort == @word.split("").sort}
+        
+        # this other method works its just more verbose
+
+        # array.each do |value|
+        #     if (value.split("").sort == @word.split("").sort)
+        #         puts value
+        #     end
+        # end
+    end   
+end
+
+# listen = Anagram.new("listen")
+# listen.match(%w[enlists google inlets banana])
